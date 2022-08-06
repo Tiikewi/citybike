528f6bddb3c4c3b63d4f34e5b91b85fa2f5912b9
Kimi <porthan.kimi@gmail.com>
2022-08-07 00:20:28 +0300

    create react app and reformat code

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import JourneyList from "./JourneyList";
+
+function App() {
+  return (
+    <div>
+      <JourneyList></JourneyList>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/JourneyList.js
@@ -0,0 +1,9 @@
+const JourneyList = () => {
+  return (
+    <div>
+      <h1>Journeys</h1>
+    </div>
+  );
+};
+
+export default JourneyList;
