f24868c46c79bb0fdeb269d5f7ba2b86a0b05953
Kimi <porthan.kimi@gmail.com>
2022-08-07 13:05:52 +0300

    fix to consistent json naming

--- backend/journey.go
+++ backend/journey.go
@@ -6,7 +6,7 @@ type Journey struct {
 	RetTime        string `json:"returnTime"`
 	DepStationId   int    `json:"departureStationId"`
 	DepStationName string `json:"departureStationName"`
-	RetStationId   int    `json:"retStationId"`
+	RetStationId   int    `json:"returnStationId"`
 	RetStationName string `json:"returnStationName"`
 	Distance       int    `json:"distance"`
 	Duration       int    `json:"duration"`
