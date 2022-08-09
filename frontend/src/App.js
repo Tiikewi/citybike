4c753403deefee91d1629f0c775ff46f804b32a6
Kimi <porthan.kimi@gmail.com>
2022-08-09 20:15:50 +0300

    add dummy home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import JourneyList from "./JourneyList";
 import MyNavbar from "./MyNavbar";
 import StationList from "./StationList";
 import SingleStation from "./SingleStation";
+import Home from "./Home";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <div>
         <MyNavbar />
         <Routes>
-          <Route path="/" element={<h1>home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/journeys" element={<JourneyList />} />
           <Route path="/stations" element={<StationList />} />
           <Route path="stations/:id" element={<SingleStation />} />
---
+++ frontend/src/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Usage</h3>
+      <span>
+        Display journeys and stations from navbar links above. <br /> You can
+        navigate tables using arrow keys, or by buttons on bottom right corner
+        of the table.
+      </span>
+      <hr />
+      <h3>Todos</h3>
+      <span>
+        <ul>
+          <li>Make code more modular.</li>
+          <li>Improve api calls on front.</li>
+          <li>Implement better error handling, especially on frontend.</li>
+          <li>Implement more features.</li>
+        </ul>
+      </span>
+    </div>
+  );
+};
+
+export default Home;
